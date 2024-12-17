9b4cb51cf83f20f11a689dc54a06f0d517c9e1eb
2100562 <2100562@estudante.uab.pt>
2024-12-17 10:51:55 +0000

    Added Swagger UI and OpenAPI generator

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const port = process.env.PORT || 8081;
 const host = process.env.HOST || '0.0.0.0';
 const express = require('express');
 const app = express();
+const expressOasGenerator = require('express-oas-generator');
 
 app.use('/assets', express.static('assets'));
 app.use('/views', express.static('views'));
@@ -10,6 +11,7 @@ app.use('/views', express.static('views'));
 require('./init/db.js')(app, () => {
   require('./init/middleware')(app);
   require('./init/router')(app);
+  expressOasGenerator.init(app, {});
   app.listen(port, host, (error) => {
     if (error) throw error;
     console.log('Your app is listening on ' + port);
