2fe4b7aaf45b4af80fb9c1fa5b304d37e02cdf6d
2100562 <2100562@estudante.uab.pt>
2024-12-20 09:35:04 +0000

    Update frontend to use new expert/sponsor routes

--- frontend/src/api/expert.service.js
+++ frontend/src/api/expert.service.js
@@ -1,24 +1,87 @@
-import { userService } from "./user.service";
+import API_URL from "@/api/config";
 
 export const expertService = {
   async getExperts(token) {
-    return userService.getUsers(token).then(res => {
-      res.body = res.body.filter(user => user.isExpert);
-      return res;
+    const response = await fetch(`${API_URL}/experts`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token
+      }
     });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw Error(handleResponses(response.status));
+    }
   },
 
   async addExpert(token, payload) {
-    return userService.editUser(token, payload);
+    const response = await fetch(`${API_URL}/experts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token
+      },
+      body: JSON.stringify(payload)
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw Error(handleResponses(response.status));
+    }
   },
 
   async editExpert(token, payload) {
-    return userService.editUser(token, payload);
+    const response = await fetch(`${API_URL}/experts/${payload._id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token
+      },
+      body: JSON.stringify(payload)
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw Error(handleResponses(response.status));
+    }
   },
 
   async removeExpert(token, id) {
-    return userService.removeUser(token, id);
+    const response = await fetch(`${API_URL}/experts/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token
+      }
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw Error(handleResponses(response.status));
+    }
   }
 };
+
+function handleResponses(code) {
+  let message = "";
+  switch (code) {
+    case 401:
+      message = "Não está autorizado a executar esta ação!";
+      break;
+    case 406:
+      message =
+        "Ação não aceitável. Verifique se preencheu bem todos os campos!";
+      break;
+    case 409:
+      message = "Já existe um especialista com este username!";
+      break;
+    default:
+      message = "Mensagem desconhecida";
+      break;
+  }
+  return message;
+}
 
 export default expertService;
--- frontend/src/api/sponsor.service.js
+++ frontend/src/api/sponsor.service.js
@@ -1,24 +1,87 @@
-import { userService } from "./user.service";
+import API_URL from "@/api/config";
 
 export const sponsorService = {
   async getSponsors(token) {
-    return userService.getUsers(token).then(res => {
-      res.body = res.body.filter(user => user.isSponsor);
-      return res;
+    const response = await fetch(`${API_URL}/sponsors`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token
+      }
     });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw Error(handleResponses(response.status));
+    }
   },
 
   async addSponsor(token, payload) {
-    return userService.addUser(token, payload);
+    const response = await fetch(`${API_URL}/sponsors`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token
+      },
+      body: JSON.stringify(payload)
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw Error(handleResponses(response.status));
+    }
   },
 
   async editSponsor(token, payload) {
-    return userService.editUser(token, payload);
+    const response = await fetch(`${API_URL}/sponsors/${payload._id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token
+      },
+      body: JSON.stringify(payload)
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw Error(handleResponses(response.status));
+    }
   },
 
   async removeSponsor(token, id) {
-    return userService.removeUser(token, id);
+    const response = await fetch(`${API_URL}/sponsors/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token
+      }
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw Error(handleResponses(response.status));
+    }
   }
 };
+
+function handleResponses(code) {
+  let message = "";
+  switch (code) {
+    case 401:
+      message = "Não está autorizado a executar esta ação!";
+      break;
+    case 406:
+      message =
+        "Ação não aceitável. Verifique se preencheu bem todos os campos!";
+      break;
+    case 409:
+      message = "Já existe um patrocinador com este username!";
+      break;
+    default:
+      message = "Mensagem desconhecida";
+      break;
+  }
+  return message;
+}
 
 export default sponsorService;
